c8082f50702fc8dfc19a4787c82204a7f299f98b
Olivia Mei <68489056+olivesmoo@users.noreply.github.com>
2023-02-02 14:48:42 -0500

    Added convert to OkLab

--- convert.js
+++ convert.js
@@ -1,20 +1,14 @@
+
 function rgb255torgb1(a)
 {
 	return [a[0]/255, a[1]/255, a[2]/255];
 }
 function rgbtolin(C_srgb)
 {
-	//return [a[0]**2.2, a[1]**2.2, a[2]**2.2];
 	let out = [];
 	a = 0.055;
 	if(C_srgb < 0.04045)
 	{
-		/*
-		linear_portion = 1;
-		out[linear_portion] = C_srgb[linear_portion] / 12.92;
-		out[0] = ((C_srgb[0] + a) / (a + 1)) ** 2.4
-		out[2] = ((C_srgb[2] + a) / (a + 1)) ** 2.4
-		*/
 		out[0] =  C_srgb[0] / 12.92;
 		out[1] = C_srgb[1] / 12.92;
 		out[2] = C_srgb[2] / 12.92;
@@ -41,6 +35,24 @@ function lintocvd(mat2)
 	return [r,g,b];
 }
 
+function linToOk(c)
+{
+	l = 0.4122214708 * c[0] + 0.5363325363 * c[1] + 0.0514459929 * c[2];
+	m = 0.2119034982 * c[0] + 0.6806995451 * c[1] + 0.1073969566 * c[2];
+	s = 0.0883024619 * c[0] + 0.2817188376 * c[1] + 0.6299787005 * c[2];
+
+    l_ = l**(1/3);
+    m_ = m**(1/3);
+    s_ = s**(1/3);
+
+    return [
+		0.2104542553*l_ + 0.7936177850*m_ - 0.0040720468*s_,
+        1.9779984951*l_ - 2.4285922050*m_ + 0.4505937099*s_,
+        0.0259040371*l_ + 0.7827717662*m_ - 0.8086757660*s_,
+	]
+       
+}
+
 	color1 = [100,23,210];
 	color2 = rgb255torgb1(color1);
 	console.log(color2);
@@ -48,3 +60,5 @@ function lintocvd(mat2)
 	console.log(color3);
 	color4 = lintocvd(color3);
 	console.log(color4);
+	color5 = linToOk(color3);
+	console.log(color5);
